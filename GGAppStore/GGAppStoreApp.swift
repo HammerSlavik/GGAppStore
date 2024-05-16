1aff2c1ed49507059f083ee6a43ae400218a734f
Sviatoslav Yakobchuk <s.yakobchuk@viseven.com>
2024-05-16 20:38:31 +0300

    Initial commit

---
+++ GGAppStore/GGAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GGAppStoreApp.swift
+//  GGAppStore
+//
+//  Created by Sviatoslav Yakobchuk on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GGAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
